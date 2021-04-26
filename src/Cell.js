746556ac062a3d573e857a25cf5c74da72a9c8a8
glennanj1 <glennan20@gmail.com>
2021-04-26 18:26:32 -0400

    commit tree 492f207531734c953c869403a815931b45bcc910

---
+++ src/Cell.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+class Cell extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            color: props.value
+        }
+    }
+
+    eventHandler = () => {
+        this.setState({
+            color: '#333'
+        })
+    }
+
+    render() {
+        return(
+            <div onClick={this.eventHandler} className='cell' style={{backgroundColor: this.state.color}}></div>
+        )
+    }
+}
+
+export default Cell
